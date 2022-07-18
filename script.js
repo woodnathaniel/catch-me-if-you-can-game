bbe33c3e998e721eea0b81e4f381b60584349719
woodnathaniel <82489020+woodnathaniel@users.noreply.github.com>
2022-07-18 03:55:53 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const btn = document.querySelector('button');
+btn.addEventListener('mouseover', function(){
+const height = Math.floor(Math.random() * window.innerHeight)
+const width = Math.floor(Math.random() * window.innerHeight)
+btn.style.left = `${height}px`;
+btn.style.top = `${width}px`;
+document.body.classList.add('addition', 'addition1');
+})
+
+btn.addEventListener('click', function(){
+    btn.innerText = 'oop!! you got me'
+    document.body.classList.add('special');
+})
+
+setTimeout(function(){
+
+    window.close()
+}, 60000)
