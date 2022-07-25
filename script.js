95324d16498798dfd490b840c9d3dcd12d312e25
woodnathaniel <82489020+woodnathaniel@users.noreply.github.com>
2022-07-25 18:53:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+// Starting up the game...
+// pressing the 'Enter' key to start the game...
+window.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+// if the 'Enter' is pressed...
+ document.querySelector('h1').classList.add('hideH1')
+       const btn = document.createElement('button');
+       btn.innerHTML = 'Try Clicking Me';
+       document.body.appendChild(btn);
+    btn.addEventListener('mouseover', function(){
+        const height = Math.floor(Math.random() * window.innerHeight)
+        const width = Math.floor(Math.random() * window.innerWidth)
+        btn.style.left = `${height}px`;
+        btn.style.top = `${width}px`;
+        document.body.classList.add('addition');
+        })
+        // When clicked or caught...
+        btn.addEventListener('click', function(){
+            btn.innerText = 'oop!! you got me'
+            document.body.classList.add('special').remove('addtion');
+        })
+        // Game Over...
+        setTimeout(function(){
+        
+            window.close()
+        }, 60000)
+    }
+    
+  
 
-const btn = document.querySelector('button');
-btn.addEventListener('mouseover', function(){
-const height = Math.floor(Math.random() * window.innerHeight)
-const width = Math.floor(Math.random() * window.innerHeight)
-btn.style.left = `${height}px`;
-btn.style.top = `${width}px`;
-document.body.classList.add('addition');
-})
-
-btn.addEventListener('click', function(){
-    btn.innerText = 'oop!! you got me'
-    document.body.classList.add('close');
-})
-
-setTimeout(function(){
-
-    window.close()
-}, 60000)
+    })
+    
